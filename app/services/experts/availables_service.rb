435c2ea514f7da23f9c4c151736eb4af37980627
David Mauricio Gomez Zapata <davidmaogomezz@gmail.com>
2022-08-18 16:20:14 -0500

    finish build availables service

--- app/services/experts/availables_service.rb
+++ app/services/experts/availables_service.rb
@@ -17,5 +17,25 @@ module Experts
     rescue StandardError => _e
       0
     end
+
+    def calculate_load(turn)
+      cweek = turn.date_hour.strftime('%-V').to_i
+      start_week = calculate_start_week(turn.date_hour.year, cweek)
+      end_week = start_week + 6.days
+      end_week = start_week.end_of_week
+      object_load = []
+      users.each { |user| object_load << { user: user, load: user.turns.where(date_hour: start_week..end_week).size } }
+      object_load
+    rescue StandardError => _e
+      []
+    end
+
+    def calculate_start_week(year, cweek)
+      Date.parse("#{year}W#{cweek}")
+    end
+
+    def extract_cweek(turn)
+      turn.date_hour.strftime('%-V').to_i
+    end
   end
 end
